d9b12269476ea47e7c748dc78b06f1707c142da7
diogenesofweb <diogenesofweb@gmail.com>
2019-01-31 17:58:03 +0200

    netlify

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers/Reducer";
@@ -14,3 +15,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+serviceWorker.unregister();
